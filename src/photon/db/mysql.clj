7d581e91e9d0e7521478be6f065470afd3ceb4a3
defpunk (aka David Oliver) <david.oliver@qwickr.com>
2016-06-20 21:49:18 +0100

    implemented missing protocol methods.

--- src/photon/db/mysql.clj
+++ src/photon/db/mysql.clj
@@ -6,13 +6,25 @@
     )
   )
 
-(def create-table-sql "CREATE TABLE IF NOT EXISTS event (order_id BIGINT NOT NULL,stream_name VARCHAR(100) NOT NULL,event_type VARCHAR(100),caused_by VARCHAR(100), caused_by_relation VARCHAR(100), payload  BLOB, service_id  VARCHAR(100),schema_url  VARCHAR(100), PRIMARY KEY (order_id, stream_name))")
+(def page-size 500)
+
+(def create-table-sql "CREATE TABLE IF NOT EXISTS event (order_id BIGINT NOT NULL,stream_name VARCHAR(100) NOT NULL,event_type VARCHAR(100),caused_by VARCHAR(100), caused_by_relation VARCHAR(100), payload JSON, service_id  VARCHAR(100),schema_url  VARCHAR(100), event_time BIGINT, PRIMARY KEY (order_id, stream_name))")
 (def drop-table-sql "DROP TABLE event")
 
+(def trunc-event-sql "TRUNCATE TABLE event")
 
-(def insert-event-sql "INSERT INTO event (order_id BIGINT NOT NULL,stream_name VARCHAR(100) NOT NULL,event_type VARCHAR(100),caused_by VARCHAR(100), caused_by_relation VARCHAR(100), payload  BLOB, service_id  VARCHAR(100),schema_url  VARCHAR(100), PRIMARY KEY (order_id, stream_name))")
+(def insert-event-sql "INSERT INTO event (order_id,stream_name ,event_type,
+caused_by , caused_by_relation, payload, service_id ,schema_url, event_time  ) VALUES (?,?,?,?,?,?,?,?,?)")
 
+(def find-event-by-id-sql  "SELECT order_id, stream_name, event_type, caused_by, caused_by_relation, payload, service_id, schema_url, event_time FROM event where order_id = ?")
+(def find-event-by-id-and-stream-sql  "SELECT order_id, stream_name, event_type, caused_by, caused_by_relation, payload, service_id, schema_url , event_time FROM event where order_id = ? and stream_name = ?")
 
+(def delete-event-by-id-sql "DELETE FROM event where order_id = ?")
+
+(def select-all-stream-names-sql "SELECT DISTINCT stream_name from event")
+
+(def get-paged-events-for-stream-sql "SELECT order_id, stream_name, event_type, caused_by, caused_by_relation, payload, service_id, schema_url , event_time FROM event where  stream_name = ? AND order_id >= ? LIMIT ? OFFSET ? ")
+(def get-paged-events-sql "SELECT order_id, stream_name, event_type, caused_by, caused_by_relation, payload, service_id, schema_url , event_time FROM event where order_id >= ? LIMIT ? OFFSET ? ")
 
 (defn conf->db-spec [conf]
   {:classname   "com.mysql.cj.jdbc.Driver"
@@ -24,7 +36,17 @@
   )
 
 (defn save-event [db-spec event]
-  (jdbc/execute! db-spec [])
+  (jdbc/execute! db-spec [insert-event-sql
+                          (:order-id event)
+                          (:stream-name event)
+                          (:event-type event)
+                          (:caused-by event)
+                          (:caused-by-relation event)
+                          (:payload event)
+                          (:service-id event)
+                          (:schema-url event)
+                          (:event-time event)
+                          ])
   )
 
 (defn setup-tables [db-spec]
@@ -44,21 +66,83 @@
 
 (def setup-my-sql (memoize build-my-sql))
 
-(defn find-event [dbspec stream-name id]
-  nil
+(defn db-event->map [e]
+  {:order-id (:order_id e)
+   :stream-name (:stream_name e)
+   :event-type (:event_type e)
+   :caused-by (:caused_by e)
+   :caused-by-relation (:caused_by_relation e)
+   :payload (:payload e)
+   :service-id (:service_id e)
+   :event-time (:event_time e)
+   :schema-url (:schema_url e)}
   )
 
-(defn find-event-by-id [dbspec id]
+(defn find-event-in-any-stream [db-spec id]
+  (let [event-result
+        (jdbc/query db-spec [find-event-by-id-sql id])]
 
+    (if (first event-result)
+      (db-event->map (first event-result))
+      nil
+      )
+    )
   )
 
-(defn delete-event [dbspec stream-name id]
+(defn find-event-in-stream [db-spec stream-name id]
+  (let [event-result
+        (jdbc/query db-spec [find-event-by-id-and-stream-sql id stream-name])]
 
+    (if (first event-result)
+        (db-event->map (first event-result))
+        nil
+      )
+    )
   )
 
-(defn delete-all-events [dbspec stream-name id]
-
+(defn find-event [db-spec stream-name id]
+  (if (or (= :__all__ stream-name)
+          (= "__all__" stream-name))
+      (find-event-in-any-stream db-spec id)
+      (find-event-in-stream db-spec stream-name id)
+    )
   )
+
+(defn find-event-by-id [db-spec id]
+  (map db-event->map (jdbc/query db-spec [find-event-by-id-sql id]))
+  )
+
+(defn delete-event [db-spec id]
+  (jdbc/execute! db-spec [delete-event-by-id-sql id])
+  )
+
+(defn event-count [db-spec]
+  (:ec (first (jdbc/query db-spec ["SELECT COUNT(*) as ec from event"])) ))
+
+
+(defn delete-all-events [db-spec]
+  (jdbc/execute! db-spec [trunc-event-sql])
+  )
+
+(defn all-stream-names [db-spec]
+  (let [result
+        (jdbc/query db-spec [select-all-stream-names-sql])]
+     (map #({:stream-name (get % :stream_name)})  result)
+    )
+  )
+
+(defn get-events [db-spec stream-name starting-order-id page page-size]
+  (let [offset (* page-size page)]
+
+    (if (or (= :__all__ stream-name)
+            (= "__all__" stream-name))
+      (jdbc/query db-spec [get-paged-events-sql starting-order-id page-size offset])
+      (jdbc/query db-spec [get-paged-events-for-stream-sql stream-name starting-order-id page-size offset])
+
+      ))
+  )
+
+
 
 (defrecord MySQLDB [conf]
   db/DB
@@ -73,7 +157,7 @@
     (let [db-spec (setup-my-sql conf)] (delete-all-events db-spec))
     )
   (search [this id] (let [db-spec (setup-my-sql conf)] (find-event-by-id db-spec id)))
-  (distinct-values [this k] (println "finding distinct values for " k))
+
   (store [this payload]
     (log/trace "Payload" payload)
     (let [event-time (:event-time payload)
@@ -82,31 +166,35 @@
                                                  (System/currentTimeMillis)
                                                  (long event-time)))]
 
+      (let [db-spec (setup-my-sql conf)]
+        (save-event db-spec new-payload)
+        )
       )
     )
 
-  (lazy-events [this stream-name date] "no events yet")
-  (lazy-events-page [this stream-name date page] "need to find all the events next")
+
+  (distinct-values [this k]
+    (into #{}
+          (map #(get % k)
+               (let [db-spec (setup-my-sql conf)]
+                 (all-stream-names db-spec)
+                 )
+               ))
+
+    )
+
+  (lazy-events [this stream-name date]
+    (map db-event->map (db/lazy-events-page this stream-name date 0)))
+
+
+  (lazy-events-page [this stream-name date page]
+    (let [oid (if (nil? date) 0 (* 1000 date))
+          results (let [db-spec (setup-my-sql conf)] (get-events db-spec stream-name oid page page-size))
+          ]
+      (when-let [r (seq results)]
+        (lazy-cat r
+                  (db/lazy-events-page this stream-name date
+                                       (+ page page-size)))))
+
+    )
   )
-
-;(db/store [this payload]
-;          (log/trace "Payload" payload)
-;          (let [event-time (:event-time payload)
-;                new-payload (assoc (into {} payload) :event-time
-;                                                     (if (nil? event-time)
-;                                                       (System/currentTimeMillis)
-;                                                       (long event-time)))]
-;            (with-open [w (clojure.java.io/writer (file-name conf) :append true)]
-;              (.write w (str (json/generate-string new-payload) "\n")))))
-
-;(lazy-events-page [this stream-name date page]
-;                  (m/with-mongo (mongo conf)
-;                                (let [l-date (if (string? date) (read-string date) date)
-;                                      res (m/fetch collection :where {:stream-name stream-name}
-;                                                   :skip (* page-size page) :limit page-size)]
-;                                  (log/info "Calling mongo: " :where {:stream-name stream-name}
-;                                            :skip (* page-size page) :limit page-size)
-;                                  (if (< (count res) 1)
-;                                    []
-;                                    (concat res
-;                                            (lazy-seq (db/lazy-events-page this stream-name l-date (inc page))))))))
--- test/photon/db/mysql_test.clj
+++ test/photon/db/mysql_test.clj
@@ -2,23 +2,22 @@
   (:require
     [clojure.test :refer :all]
     [photon.db.mysql
-             :refer
-             :all] )
+     :refer
+     :all])
   )
-
 
 (deftest can-create-config
   (testing "can create dbspec from conf"
-           (is (= {:classname   "com.mysql.cj.jdbc.Driver"
-                   :subprotocol "mysql"
-                   :subname     "testhost:3306/db"
-                   :user        "user"
-                   :password "password"}
+    (is (= {:classname   "com.mysql.cj.jdbc.Driver"
+            :subprotocol "mysql"
+            :subname     "testhost:3306/db"
+            :user        "user"
+            :password    "password"}
 
-                  (conf->db-spec {:mysql.connection-url "testhost:3306/db"
-                                  :mysql.user           "user"
-                                  :mysql.password "password"})))
-           )
+           (conf->db-spec {:mysql.connection-url "testhost:3306/db"
+                           :mysql.user           "user"
+                           :mysql.password       "password"})))
+    )
   )
 
 ;The integration tests assume that a debase named eventstore
@@ -28,23 +27,119 @@
 (def my-sql-host "192.168.99.100")
 
 (deftest db-functions-integration-test
-  (let [db-spec  {:classname   "com.mysql.cj.jdbc.Driver"
-                  :subprotocol "mysql"
-                  :subname     (str "//" my-sql-host "/eventstore")
-                  :user        "photon"
-                  :password "ph0t0n" } ]
+  (let [db-spec {:classname   "com.mysql.cj.jdbc.Driver"
+                 :subprotocol "mysql"
+                 :subname     (str "//" my-sql-host "/eventstore")
+                 :user        "photon"
+                 :password    "ph0t0n"}]
 
+    (testing "clean up before testing" (drop-tables db-spec))
     (testing "can create schema" (setup-tables db-spec))
 
     (testing "should not be able to find an event thats yet to exist"
       (= nil (find-event db-spec "test-stream" 1)))
 
     (testing "create and look up an event"
-      ()
-      (= nil (find-event db-spec "test-stream" 1)))
+      (save-event db-spec {:order-id    1
+                           :stream-name "test-stream"
+                           :event-type  "test"
+                           :caused-by   "test-suite"
+                           :payload     "{}"
+                           :service-id  "testing"
+                           :event-time  100})
+      (is (= false (nil? (find-event db-spec "test-stream" 1))))
+      (is (= false (nil? (find-event-by-id db-spec 1))))
 
-    (testing "shoudld be able to create an event")
+      )
+
+    (testing "saved event is mapped back correctly"
+      (let [event (find-event db-spec "test-stream" 1)]
+        (is (map? event))
+        (is (= {:order-id           1
+                :stream-name        "test-stream"
+                :event-type         "test"
+                :caused-by          "test-suite"
+                :caused-by-relation nil
+                :payload            "{}"
+                :schema-url         nil
+                :event-time         100
+                :service-id         "testing"} event))
+
+        (is (= event (find-event db-spec "__all__" 1)))
+        (is (= event (find-event db-spec :__all__ 1)))
+        )
+      )
+
+
+    (testing "look up and search should return same result when only one order id exists"
+      (is (= 1 (count (find-event-by-id db-spec 1))))
+      (is (= (find-event db-spec "test-stream" 1) (first (find-event-by-id db-spec 1)))
+          )
+      )
+
+    (testing "can delete an event"
+      (delete-event db-spec 1)
+      (is (empty? (find-event db-spec "test-stream" 1)))
+      )
+
+    (testing "can delete all events"
+      ;create alot of events
+      (dotimes [n 50] (save-event db-spec {:order-id    n
+                                           :stream-name "test-stream"
+                                           :event-type  "test"
+                                           :caused_by   "test-suite"
+                                           :payload     "{}"
+                                           :service-id  "testing"}))
+
+      (is (= 50 (event-count db-spec)))                     ;check events created
+      (delete-all-events db-spec)
+      (is (= 0 (event-count db-spec)))                      ; check no events left
+
+      )
+
+    (testing "can page events"
+      ;create alot of events
+      (dotimes [n 50] (save-event db-spec {:order-id    (+ 1 n)
+                                           :stream-name "test-stream"
+                                           :event-type  "test"
+                                           :caused_by   "test-suite"
+                                           :payload     "{}"
+                                           :service-id  "testing"}))
+
+      (is (= 10 (count (get-events db-spec "test-stream" 1 1 10))))
+      (is (= (range 1 11) (map #(:order_id %) (get-events db-spec "test-stream" 1 0 10))))
+      (is (= (range 11 21) (map #(:order_id %) (get-events db-spec "test-stream" 1 1 10))))
+      (is (= (range 26 51) (map #(:order_id %) (get-events db-spec "test-stream" 1 1 25))))
+
+      )
+
+    (testing "can page events across streams"
+      (delete-all-events db-spec)
+      ;create alot of events
+      (dotimes [n 50]
+        (save-event db-spec {:order-id    (+ 1 (* 2 n))
+                             :stream-name "stream-two"
+                             :event-type  "test"
+                             :caused_by   "test-suite"
+                             :payload     "{}"
+                             :service-id  "testing"})
+
+        (save-event db-spec {:order-id    (+ 2 (* 2 n))
+                             :stream-name "stream-one"
+                             :event-type  "test"
+                             :caused_by   "test-suite"
+                             :payload     "{}"
+                             :service-id  "testing"})
+
+        )
+
+      (is (= 10 (count (get-events db-spec "stream-two" 1 1 10))))
+      (is (= (filter odd? (range 1 21)) (map #(:order_id %) (get-events db-spec "stream-two" 1 0 10))))
+      (is (= (filter even? (range 1 21)) (map #(:order_id %) (get-events db-spec "stream-one" 1 0 10))))
+      (is (= (range 1 11) (map #(:order_id %) (get-events db-spec "__all__" 1 0 10))))
+      (is (= (range 1 11) (map #(:order_id %) (get-events db-spec :__all__ 1 0 10))))
+      )
 
     ;(testing "can clean up"  (drop-tables db-spec))
     )
-   )
+  )
